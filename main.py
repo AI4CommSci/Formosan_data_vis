ce22ecf854cf88e7785bab5b8f14b5395735b29b
Wael Mohamed <wael@Waels-Air.ht.home>
2024-09-03 11:17:54 -0400

    update numbers

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from matplotlib.gridspec import GridSpec
 
 def main():
     # Sample data
-    labels = ['Amis\n(1774594)', 'Atayal\n(892089)', 'Bunun\n(155330)', 'Kanakanavu\n(48463)', 'Kavalan\n(74295)', 'Paiwan\n(193519)', 
-              'Puyuma\n(125494)', 'Rukai\n(146324)', 'Saaroa\n(26758)', 'Saisiyat\n(55910)', 'Sakizaya\n(423935)', 'Seediq\n(1317978)', 
-              'Taroko\n(41574)', 'Thao\n(53362)', 'Tsou\n(25238)', 'Yami\n(47093)']
-    sizes = [1774594, 892089, 155330, 48463, 74295, 193519, 125494, 146324, 26758, 55910, 423935, 1317978, 41574, 53362, 25238, 47093]    
+    labels = ['Amis\n(1785997)', 'Atayal\n(899827)', 'Bunun\n(133907)', 'Kanakanavu\n(64961)', 'Kavalan\n(87461)', 'Paiwan\n(200714)', 
+              'Puyuma\n(133483)', 'Rukai\n(152891)', 'Saaroa\n(31443)', 'Saisiyat\n(64442)', 'Sakizaya\n(456174)', 'Seediq\n(1324932)', 
+              'Taroko\n(45444)', 'Thao\n(62030)', 'Tsou\n(44438)', 'Yami\n(59723)']
+    sizes = [1785997, 899827, 133907, 64961, 87461, 200714, 133483, 152891, 31443, 64442, 456174, 1324932, 45444, 62030, 44438, 59723]    
     sizes = [math.sqrt(x) for x in sizes]
     # Normalize the sizes to sum up to 1
     sizes_normalized = squarify.normalize_sizes(sizes, 10, 20)
@@ -37,26 +37,26 @@ def main():
     # # Plot the treemap
     plt.figure(figsize=(10, 11))
 
-    plot1 = plt.subplot2grid((10, 11), (0, 4), colspan=7, rowspan=8)
-    plot2 = plt.subplot2grid((10, 11), (0, 0), colspan=4, rowspan=3)
-    plot3 = plt.subplot2grid((10, 11), (3, 0), colspan=4, rowspan=3)
-    plot4 = plt.subplot2grid((10, 11), (6, 0), colspan=4, rowspan=2)
+    plot1 = plt.subplot2grid((10, 12), (0, 4), colspan=8, rowspan=8)
+    plot2 = plt.subplot2grid((10, 12), (0, 0), colspan=4, rowspan=3)
+    plot3 = plt.subplot2grid((10, 12), (3, 0), colspan=4, rowspan=3)
+    plot4 = plt.subplot2grid((10, 12), (6, 0), colspan=4, rowspan=2)
 
     plot1.axis('off')
-    plot1.set_title('Words scraped per Formosan language (5401956 total)')
-    squarify.plot(sizes=sizes_normalized, label=labels, color=color_palette, alpha=.8, ax=plot1)
+    plot1.set_title('Words scraped per Formosan language (5547867 total)', fontsize=16)
+    squarify.plot(sizes=sizes_normalized, label=labels, color=color_palette, alpha=.8, ax=plot1, text_kwargs={'fontsize': 14})
 
-    plot2.set_title('Francis & Kucera Corpus')
+    plot2.set_title('Francis & Kucera Corpus', fontsize=16)
     plot2.axis('off')
-    squarify.plot(sizes=[1], label=['~ 1 Million Words'], alpha=.8, ax=plot2)
+    squarify.plot(sizes=[1], label=['~ 1 Million Words'], alpha=.8, ax=plot2, text_kwargs={'fontsize': 14})
 
-    plot3.set_title('Penn Treebank - WSJ section')
+    plot3.set_title('Penn Treebank - WSJ section', fontsize=16)
     plot3.axis('off')
-    squarify.plot(sizes=[1], label=['~ 1 Million Words'], alpha=.8, ax=plot3)
+    squarify.plot(sizes=[1], label=['~ 1 Million Words'], alpha=.8, ax=plot3, text_kwargs={'fontsize': 14})
 
-    plot4.set_title('Adam, Eve, and Sarah Corpus')
+    plot4.set_title('Adam, Eve, and Sarah Corpus', fontsize=16)
     plot4.axis('off')
-    squarify.plot(sizes=[1], label=['672,429 Words'], alpha=.8, ax=plot4)
+    squarify.plot(sizes=[1], label=['672,429 Words'], alpha=.8, ax=plot4, text_kwargs={'fontsize': 14})
 
 
     plt.tight_layout()
